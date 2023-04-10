f92d42ba184ec24e1f1e8f293a0050eeffc41301
Trang Nguyen <tnguyenngoc@rbbn.com>
2023-04-10 16:46:51 +0700

    no message

--- jenkinsfile/mylibrary.js
+++ jenkinsfile/mylibrary.js
@@ -1,7 +1,5 @@
 // @Library('mylibrary') _
-def libraryPath = "${WORKSPACE}/mylibrary.js"
-def cl = this.class.classLoader
-cl.addURL(new URL("file://${libraryPath}/"))
+
 pipeline {
     agent any
 
@@ -9,9 +7,13 @@ pipeline {
         stage('Say Hello') {
             steps {
                 script {
-                    def greeting = MyLibrary.sayHello('Jenkins')
-                    echo greeting
+
 					echo "test NNT"
+					def libraryPath = "${WORKSPACE}/mylibrary.js"
+					def cl = this.class.classLoader
+					cl.addURL(new URL("file://${libraryPath}/"))
+					def greeting = MyLibrary.sayHello('Jenkins')
+                    echo greeting
                 }
             }
         }
