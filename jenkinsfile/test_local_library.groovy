a1cfa9a07990812dcc427c5eaba92ff4876b1caf
Trang Nguyen <tnguyenngoc@rbbn.com>
2023-04-11 10:09:16 +0700

    test

--- jenkinsfile/test_local_library.groovy
+++ jenkinsfile/test_local_library.groovy
@@ -10,7 +10,7 @@ pipeline {
                     userRemoteConfigs: [[url: 'https://github.com/ngoctrang2206/hello-world.git']]
                 ])
                 // Load the file from the repository
-                def myScript = load '$WORKSPACE/var/myfile.groovy'
+                def myScript = load "$WORKSPACE/var/myfile.groovy"
                 // Call a function in the loaded script
                 myScript.myFunction()
             }
